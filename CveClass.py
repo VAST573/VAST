eec58bf0ae7bae289218cb6fab43d106364db3ab
Ubuntu <ubuntu@ip-172-31-52-92.ec2.internal>
2020-03-01 04:13:08 +0000

    Parse json file now gets the published dated and modified date as datetime objects. Cve class now holds that new information

--- CveClass.py
+++ CveClass.py
@@ -5,11 +5,12 @@
 
 class Cve:
     # init method that constructs a instance of a cve
-    def __init__(self, cveIdNumber, impactScoreV2, impactScoreV3, lastPublishedDate, description):
+    def __init__(self, cveIdNumber, impactScoreV2, impactScoreV3, lastPublishedDate, lastModifiedDate, description):
         self.cveIdNumber = cveIdNumber
         self.impactScoreV2 = impactScoreV2
         self.impactScoreV3 = impactScoreV3
         self.lastPublishedDate = lastPublishedDate
+        self.lastModifiedDate = lastModifiedDate
         self.description = description
 
     # Returns the cveIdNumber for the cve as a string.
@@ -26,6 +27,10 @@ class Cve:
     # Returns the lastPublishedDate of the cve as a dateTime Object.
     def getlastPublishedDate(self):
         return self.lastPublishedDate
+
+    # Returns the last Modified date of the cve as a dateTime Object.
+    def getLastModifiedDate(self):
+        return self.lastModifiedDate
 
     # Returns the description of the cve as a string.
     def getDescription(self):
--- ParseJsonFile.py
+++ ParseJsonFile.py
@@ -35,10 +35,11 @@ def getCveInformation(nvd_json_dict):
         cve_info = cve['cve']
         cve_id_number = FindCveIdNumber(cve_info)
         cve_last_published_date = FindLastPublishedDate(cve)
+        cve_last_modified_date = FindLastModifiedDate(cve)
         cve_impact_scoreV2 = FindCveImpactScoreV2(cve)
         cve_impact_scoreV3 = FindCveImpactScoreV3(cve)
         cve_description = FindDescription(cve_info)
-        cve_object = CveClass.Cve(cve_id_number, cve_impact_scoreV2, cve_impact_scoreV3, cve_last_published_date, cve_description)
+        cve_object = CveClass.Cve(cve_id_number, cve_impact_scoreV2, cve_impact_scoreV3, cve_last_published_date, cve_last_modified_date, cve_description)
         cveList.append(cve_object)
     return cveList
 
@@ -52,8 +53,19 @@ def FindCveIdNumber(cve_info):
 
 
 # helper function to traverse the cve and returns the Last Published date for the cve
+def FindLastModifiedDate(cve):
+    cve_last_modified_date = cve['lastModifiedDate']
+    firstSplit = cve_last_modified_date.split('T')
+    SecondSplit = firstSplit[1].split('Z')
+    date_str = firstSplit[0]
+    time_str = SecondSplit[0]
+    dateTime_str = date_str +' '+ time_str
+    dateTime_object = datetime.datetime.strptime(dateTime_str, '%Y-%m-%d %H:%M')
+    return dateTime_object
+
+
 def FindLastPublishedDate(cve):
-    cve_last_published_date = cve['lastModifiedDate']
+    cve_last_published_date = cve['publishedDate']
     firstSplit = cve_last_published_date.split('T')
     SecondSplit = firstSplit[1].split('Z')
     date_str = firstSplit[0]
@@ -92,7 +104,7 @@ def FindCveImpactScoreV3(cve):
         cve_impact_score = 'No impact score available'
     return cve_impact_score
 
-    
+
 
 # the file we want to read and traverse
 recent_nvd_file = '/home/ubuntu/SWEProject/nvdFileLocation/nvdcve-1.1-recent.json'
@@ -104,5 +116,6 @@ nvd_json_dict = openFile(recent_nvd_file)
 # traverse the nvd dict to get all the cve information as a list of cve's
 cveInstanceList = getCveInformation(nvd_json_dict)
 
-print(cveInstanceList[0].getimpactScoreV2())
+print(cveInstanceList[56].getLastModifiedDate())
+print(cveInstanceList[56].getlastPublishedDate())
 
