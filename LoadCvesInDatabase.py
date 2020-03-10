7697a4689d1bf686462184d15b62ceffabf1040b
Ubuntu <ubuntu@ip-172-31-52-92.ec2.internal>
2020-03-10 15:02:35 -0400

    add cve to database method now works properly

--- LoadCvesInDatabase.py
+++ LoadCvesInDatabase.py
@@ -8,20 +8,24 @@ def addCvesToDatabase(cveInstanceList):
         cnx = mysql.connector.connect(user='cyberuser', password='cyber',
                               host='127.0.0.1',
                               database='VAST')
-        cursor = cnx.cursor()
+        cursor = cnx.cursor(buffered=True)
         cursor.execute("USE VAST") # select the database
-        cursor.execute("SHOW TABLES")    # execute 'SHOW TABLES' (but data is not returned)
-        if not cveInstanceList:
-            print("cve List is empty, no new updates")
-            cnx.close()
-        else:
-            for cve in cveInstanceList:
-                insertCVE = 'INSERT INTO CVE_Alerts (cve_alert_id, CVE_Number, description, impactV2, impactV3, publish_date, modified_date) VALUES  ({}, {}, {}, {}, {}, {}))'.format(cve.getcveIDNumber, cve.getDescription, cve.getimpactScoreV2, cve.getimpactScoreV3, cve.getlastPublishedDate, cve.getLastModifiedDate)
-                cursor.execute(insertCVE)
+        #cursor.execute("SHOW TABLES")    # execute 'SHOW TABLES' (but data is not returned)
+        for cve in cveInstanceList:
+            insertCVE = ("INSERT INTO CVE_Alerts " 
+                        "(CVE_Number, description, impactV2, impactV3, publish_date, modified_date)"
+                        "VALUES  (%s, %s, %s, %s, %s, %s)")
+            cveInfo = (cve.getcveIDNumber(), cve.getDescription(), cve.getimpactScoreV2(), cve.getimpactScoreV3(), cve.getlastPublishedDate(),cve.getLastModifiedDate())
+            cursor.execute(insertCVE,cveInfo)
         print('cves where addded to the database')
-        cnx.close()
+        cnx.commit()
+
     except mysql.connector.Error as error:
-        print("Failed to insert record into Laptop table {}".format(error))
+        print("Failed to insert record into CVE table {}".format(error))
+        print(type(error))
+
+    finally:
+        cnx.close()
 
 
 
